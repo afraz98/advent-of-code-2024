5f755c6a44ddd200b07eac08c3a13ea44769a6eb
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-03 00:43:54 -0500

    Wicked sick code golfing

--- day03/day3.py
+++ day03/day3.py
@@ -1,22 +1,13 @@
 import re
 
-def parse_input(filename):
-    return open(filename, 'r').read()
-
 def product(list):
     product = 1
     for elem in list:
         product *= elem
     return product
 
-def solve_part_one():
-    summ = 0
-    input = parse_input("day03.txt")
-    instructions = re.findall("mul\(\d{1,3},\d{1,3}\)", input)
-    for instruction in instructions:
-       summ += product([int(i) for i in instruction.strip("mul()").split(",")])
-    print(summ)
-    pass
+def solve_part_one():    
+    print(sum([product([int(i) for i in instruction.strip("mul()").split(",")]) for instruction in re.findall("mul\(\d{1,3},\d{1,3}\)", open("day03.txt", 'r').read())]))
 
 def solve_part_two():
     pass
