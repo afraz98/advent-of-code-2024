9265d663bda37376a12831511f18d86c801c4389
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-03 00:48:23 -0500

    We can do bettergit add -A !

--- day03/day3.py
+++ day03/day3.py
@@ -1,13 +1,8 @@
+from functools import reduce
 import re
 
-def product(list):
-    product = 1
-    for elem in list:
-        product *= elem
-    return product
-
 def solve_part_one():    
-    print(sum([product([int(i) for i in instruction.strip("mul()").split(",")]) for instruction in re.findall("mul\(\d{1,3},\d{1,3}\)", open("day03.txt", 'r').read())]))
+    print(sum([reduce(lambda x, y: x * y, [int(i) for i in instruction.strip("mul()").split(",")]) for instruction in re.findall("mul\(\d{1,3},\d{1,3}\)", open("day03.txt", 'r').read())]))
 
 def solve_part_two():
     pass
