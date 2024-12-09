fa5ba16d02627286dfa51f15c1144ce26f67b807
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-09 09:42:40 -0500

    Generate all possible operators

--- day06/day6.py
+++ day06/day6.py
@@ -1,5 +1,7 @@
 import timeit
 
+direction_print = ['^', '>', 'v', '<']
+
 def print_grid(grid):
     for y in range(len(grid)):
         for x in range(len(grid)):
@@ -36,7 +38,7 @@ def peek(x,y,input,direction):
 def solve_part_one():
     tiles = 0
     start_x, start_y = 0, 0
-    input = [[x for x in y] for y in parse_input("day06.txt")]
+    input = [[x for x in y] for y in parse_input("day06_test.txt")]
     direction = 0
 
     # Find starting position
@@ -55,13 +57,41 @@ def solve_part_one():
             newx, newy = find_new_coordinates(x, y, direction)
         x = newx
         y = newy
-        input[y][x] = 'X'
+        input[y][x] = direction_print[direction]
 
     for y in range(0, len(input)):
         for x in range(0, len(input[0])):
-            if input[y][x] == 'X':
+            if input[y][x] in direction_print:
                 tiles += 1
+    print_grid(input)
     print(tiles)
+
+def solve_part_two():
+    path = []
+    start_x, start_y = 0, 0
+    input = [[x for x in y] for y in parse_input("day06_test.txt")]
+    direction = 0
+
+    # Find starting position
+    for y in range(len(input)):
+        for x in range(len(input[0])):
+            if input[y][x] == '^':
+                start_x, start_y = x,y
+
+    x = start_x
+    y = start_y
+
+    while 0 <= x < len(input[0]) - 1 and 0 <= y < len(input) - 1:
+        newx, newy = find_new_coordinates(x, y, direction)
+        if not peek(newx, newy, input, direction):
+            direction = (direction + 1) % 4
+            newx, newy = find_new_coordinates(x, y, direction)
+        x = newx
+        y = newy
+        path.append((x,y))
+
 
 result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
 print("Part I ran in %s seconds" % str(result))
+result = timeit.timeit('solve_part_two()', setup='from __main__ import solve_part_two', number=1)
+print("Part II ran in %s seconds" % str(result))
--- day07/day7.py
+++ day07/day7.py
@@ -1,11 +1,28 @@
 import timeit
+from itertools import product, zip_longest, combinations
 
 def parse_input(filename):
     return [line.strip('\n') for line in open(filename, 'r')]
 
+
 def solve_part_one():
+    total = 0
+    input = parse_input("day07_test.txt")
+    for line in input:
+        lvalue, rvalue = line.split(":")
+        rvalue = [int(i) for i in rvalue.split(" ")[1:]]
+        operators = [list(x) for x in product(['+', '*'], repeat=len(rvalue)-1)] # from here: https://github.com/RD-Dev-29/advent_of_code_24/blob/main/code_files/day7.py
+        for operator in operators:
+            sequences = [item for pair in zip_longest(rvalue, operator) for item in pair if item is not None]
+            print(sequences)
+    print(total)
+    pass
+
+def solve_part_two():
     input = parse_input("day07_test.txt")
     pass
 
 result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
 print("Part I ran in %s seconds" % str(result))
+result = timeit.timeit('solve_part_two()', setup='from __main__ import solve_part_two', number=1)
+print("Part II ran in %s seconds" % str(result))
