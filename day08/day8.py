c1a9867eb4e276bf73879bfe17e1d11a26481005
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-10 00:59:02 -0500

    Day 8 part one solution

--- day08/day8.py
+++ day08/day8.py
@@ -1,10 +1,37 @@
 import timeit
 
 def parse_input(filename):
-    return [line.strip('\n') for line in open(filename, 'r')]
+    return [[x for x in y] for y in open(filename, 'r')]
+
+def get_index(input, row, col):
+    if row < 0 or row > len(input) - 1:
+        return ""
+    if col < 0 or col > len(input) - 1:
+        return ""
+    return input[row][col]
 
 def solve_part_one():
-    pass
+    antinodes = set()
+    input = parse_input("day08_test.txt")
+
+    for row in range(len(input)):
+        for col in range(len(input[0])):
+            if input[row][col] != '.': # Non-empty cell
+                for i in range(1, len(input)):
+                    for j in range(1, len(input)):
+                        if get_index(input, row + i, col + j) == input[row][col]:
+                            if get_index(input, row + i + j, col + i + j) != "":
+                                antinodes.add((row + i + j, col + i + j))
+                        if get_index(input, row - i, col - j) == input[row][col]:
+                            if get_index(input, row - i - j, col - i - j) != "":
+                                antinodes.add((row - i - j, col - i - j))
+                        if get_index(input, row + i, col - j) == input[row][col]:
+                            if get_index(input, row + i - j, col + i + j) != "":
+                                antinodes.add((row + i - j, col + i + j))
+                        if get_index(input, row - i, col + j) == input[row][col]:
+                            if get_index(input, row - i + j, col - i - j) != "":
+                                antinodes.add((row - i + j, col - i - j))
+    print(len(antinodes))
 
 def solve_part_two():
     pass
