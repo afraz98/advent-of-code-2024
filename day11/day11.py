0be509a9a81b687b11dab5ca142e06dde75e5980
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-11 01:24:41 -0500

    Day 11 part one solution

--- day11/day11.py
+++ day11/day11.py
@@ -1,25 +1,26 @@
 import timeit
 
 def parse_input(filename):
-    return [line.split(" ") for line in open(filename, 'r')][0]
+    return [[x.strip("\r\n") for x in line.split(" ")] for line in open(filename, 'r')][0]
 
 def blink(stones):
     new_stones = []
     for stone in stones:
-        print(stone)
         if stone == "0":
             new_stones.append("1")
-        elif len(stone) % 2 == 0:
-            new_stones.append(str(int(stone[0 : len(stone) // 2])))
-            new_stones.append(str(int(stone[len(stone) // 2:])))
+        elif len(stone) % 2 == 0: # Even number of digits
+            new_stones.append(str(int(stone[:(len(stone) // 2)])))
+            new_stones.append(str(int(stone[(len(stone) // 2):])))
         else:
             new_stones.append(str(int(stone) * 2024))
     return new_stones
 
 def solve_part_one():
     stones = parse_input("day11.txt")
+    print(stones)
     for _ in range(25):
         stones = blink(stones)
+        # print(stones)
     print(len(stones))
     pass
 
