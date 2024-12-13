a2847cddc86449caf3628b04b3f85ae956c70626
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-13 00:16:24 -0500

    Day 9 part one solution

--- day09/day9.py
+++ day09/day9.py
@@ -1,21 +1,38 @@
+import timeit
+
 def parse_input(filename):
     return open(filename).read().strip("\r\n")
 
 def expand_input(input):
     id = 0
-    expanded = ""
+    expanded = []
     for i in range(0, len(input)):
         if i % 2 == 0:
-            expanded += str(id) * int(input[i])
-            id += 1
+            for _ in range(int(input[i])):
+                expanded.append(str(id))
+            id = id + 1
         else:
-            expanded += "." * int(input[i])
+            for _ in range(int(input[i])):
+                expanded.append(".")
     return expanded
+
+# FIXME: Don't think I'm accounting for multi-character IDs...
 def solve_part_one():
-    print(expand_input(parse_input("day9_test.txt")))
-    pass
+    disk = expand_input(parse_input("day9.txt"))        
+    j = len(disk) - 1
+    for i in range(len(disk)):
+        if j <= i:
+            break
+        while disk[i] == "." and j > i:
+            if disk[j] !=  ".":
+                disk[j], disk[i] = disk[i], disk[j] # Swap spaces
+                j -= 1
+                break
+            j -= 1
+    print(sum([i * int(disk[i]) if disk[i] != '.' else 0 for i in range(len(disk))])) # Calculate checksum
 
 def solve_part_two():
     pass
 
-solve_part_one()
+result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
+print("Part I ran in %s seconds" % str(result))
