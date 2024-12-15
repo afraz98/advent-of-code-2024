3be1610b20573d7cd76fb97dea562e90259c4063
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-15 02:20:48 -0500

    Day 13 part two Cramer's Rule

--- day13/day13.py
+++ day13/day13.py
@@ -1,22 +1,18 @@
 import timeit
 import re
-import numpy as np # Work smart, not hard
 
 def parse_input(filename):
     return [block.split('\n') for block in open(filename, 'r').read().split("\n\n")]
 
 """
- Pick button presses such that the prize is won minimizing tokens spent.
- Each input block is a system of equations that can be solved with matrix operations.
- Solve for A,B where each variable in the number of times a button is pressed:
- 
- { X1  X1} {A} = { X }
- { Y1  Y2} {B} = { Y }
+    Cramer's Rule for a 2x2 matrix representing
+    A system of equations similar to the following:
+    {A B}{x} = {P_x}
+    {A B}{y} = {P_x}
 """
-def find_combination(button_A, button_B, prize):
-    sln = np.linalg.lstsq(np.array(list(zip(button_A, button_B))),np.array(prize))
-    print(sln[0])
-    return 3*sln[0][0] + sln[0][1]
+def cramer(a_x, b_x, a_y, b_y, p_x, p_y):
+    return ((p_x * b_y) - (p_y * b_x)) / (a_x * b_y - (a_y * b_x)), ((a_x * p_y) - (a_y * p_x)) / (a_x * b_y - (a_y * b_x))
+
 
 def solve_part_one():
     tokens = 0
@@ -25,14 +21,26 @@ def solve_part_one():
         button_a = [int(number) for number in re.findall(r"\d+", entry[0])]
         button_b = [int(number) for number in re.findall(r"\d+", entry[1])]
         prize = [int(number) for number in re.findall(r"\d+", entry[2])]
-#        tokens += find_combination(button_a, button_b, prize)
-        for i in range(100):
-            for j in range(100):
-                if i*button_a[0] + j*button_b[0] == prize[0] and i*button_a[1] + j*button_b[1] == prize[1]:
-                    tokens += (3*i + j)
+        A,B = cramer(button_a[0], button_b[0], button_a[1], button_b[1], prize[0], prize[1])
+        if 0 <= A < 100 and 0 <= B < 100:
+            if round(A) == A and round(B) == B:
+                tokens += 3*A + B
+    print(int(tokens))
+
+# Cramer's rule
+def solve_part_two():
+    tokens = 0
+    entries = parse_input("day13.txt") 
+    for entry in entries:
+        button_a = [int(number) for number in re.findall(r"\d+", entry[0])]
+        button_b = [int(number) for number in re.findall(r"\d+", entry[1])]
+        prize = [10000000000000 + int(number) for number in re.findall(r"\d+", entry[2])]
+        A,B = cramer(button_a[0], button_b[0], button_a[1], button_b[1], prize[0], prize[1])
+        if round(A) == A and round(B) == B:
+            tokens += 3*A + B
     print(tokens)
 
-def solve_part_two():
-    pass
-
-solve_part_one()
+result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
+print("Part I ran in %s seconds" % str(result))    
+result = timeit.timeit('solve_part_two()', setup='from __main__ import solve_part_two', number=1)
+print("Part II ran in %s seconds" % str(result))
