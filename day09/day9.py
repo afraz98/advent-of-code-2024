b7a15a8211cec38d0e02e35a3db825c66879867c
afraz98 <anthonymfrazier1998@gmail.com>
2024-12-19 09:40:19 -0500

    ..

--- day09/day9.py
+++ day09/day9.py
@@ -31,12 +31,56 @@ def solve_part_one():
             j -= 1
     print(sum([i * int(disk[i]) if disk[i] != '.' else 0 for i in range(len(disk))])) # Calculate checksum
 
+def expand_input_part_two(input):
+    expanded = []
+    id = 0
+    for i in range(len(input)):
+        if i % 2 == 0:
+            expanded.append([str(id) for _ in range(int(input[i]))])
+            id = id + 1
+        else:
+            expanded.append(["." for _ in range(int(input[i]))])
+    return expanded
+
+def print_disk(disk):
+    for entry in disk:
+        print(''.join(entry), end='')
+    print()
+
 # Move files (blocks of same ID) together
 def solve_part_two():
-    disk = expand_input(parse_input("day9.txt"))
+    disk = expand_input_part_two(parse_input("day9_test.txt"))
+    print_disk(disk)
+    j = len(disk) - 1
+    
+    while j > 0 and disk[j][0] != 0:
+        swap = False
+        for i in range(len(disk)):
+            print(disk[j], disk[i])
+            if j <= i:
+                break
+            if len(disk[j]) > len(disk[i]):
+                continue
+            while not swap and j > i and disk[i][0] == '.':
+                print("swap")
+                if disk[j][0] !=  ".":
+                    if len(disk[j]) == len(disk[i]): # Swap entire block
+                        disk[j], disk[i] = disk[i], disk[j]
+                        print_disk(disk)
+                        swap = True
+                    if len(disk[j]) < len(disk[i]): # Split into two regions
+                        for idx in range(len(disk[j])):
+                            disk[j][idx], disk[i][idx] = disk[i][idx], disk[j][idx]
+                        print_disk(disk)
+                        swap = True
+                j -= 1
+        print_disk(disk)
+        j -= 1
+    print_disk(disk)
+    print(sum([i * len(disk[i]) if disk[i][0] != '.' else 0 for i in range(len(disk))])) # Calculate checksum
     pass
 
-result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
-print("Part I ran in %s seconds" % str(result))
+# result = timeit.timeit('solve_part_one()', setup='from __main__ import solve_part_one', number=1)
+# print("Part I ran in %s seconds" % str(result))
 result = timeit.timeit('solve_part_two()', setup='from __main__ import solve_part_two', number=1)
 print("Part II ran in %s seconds" % str(result))
